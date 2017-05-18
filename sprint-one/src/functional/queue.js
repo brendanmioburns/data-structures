48948308ab7aa5b1292a2d75ae3377643b857171
Hack Reactor Student <student@hackreactor.com>
2017-05-17 20:51:33 -0700

    changed variable currentValue to leavingValue as well as indexOfLeavingValue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,8 +7,8 @@ var Queue = function() {
   var builder = 0;
   //size can go up and down
   var size = 0;
-  //indexOfNextToLeave is the index of the property that is next in line to be dequeued (pretends as though the last dequeue'd value was *actually* removed or popped)
-  var indexOfNextToLeave = 0;
+  //indexOfLeavingValue is the index of the property that is next in line to be dequeued (pretends as though the last dequeue'd value was *actually* removed or popped)
+  var indexOfLeavingValue = 0;
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
@@ -19,11 +19,11 @@ var Queue = function() {
 
   someInstance.dequeue = function() {
     
-    var currentValue = storage[indexOfNextToLeave];
-    indexOfNextToLeave++;
+    var leavingValue = storage[indexOfLeavingValue];
+    indexOfLeavingValue++;
     size--;
 
-    return currentValue;  
+    return leavingValue;  
   };
 
   someInstance.size = function() {
